41e1c0953daae638d4f9933e029a3cea8e077f6f
Aprajita Kumari <aprajitakumari479@gmail.com>
2023-06-15 07:05:24 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import barcode
+from barcode.writer import ImageWriter
+
+def testEan():
+    EAN = barcode.get_barcode_class('ean13')
+    ean = EAN(u'123456789011', writer=ImageWriter())
+    fullname = ean.save('my_ean13_barcode')
+
+if __name__ == '__main__':
+    testEan()
